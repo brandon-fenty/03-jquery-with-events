7181386562543a207146f8f75ec40cb657e28092
brandon-fenty <brandonfenty@gmail.com>
2018-06-07 13:49:02 -0700

    stopping point for lecture

--- kelly-brandon/scripts/articleView.js
+++ kelly-brandon/scripts/articleView.js
@@ -59,7 +59,18 @@ articleView.handleCategoryFilter = function() {
   // When an option with a value is selected, hide all the articles, then reveal the matches.
   // When the blank (default) option is selected, show all the articles, except for the template.
   // Be sure to reset the #author-filter while you are at it!
+  $('#category-filter').on('change', function() {
+    if($(this).val()) {
+      $('article').hide(500);
+      $(`article[data-category="${$(this).val()}"]`).fadeIn('500');
 
+    } else {
+      $('article').show(500);
+      $('.template').hide();
+
+    }
+    $('#author-filter').val('');
+  });
 
 };
 
@@ -67,6 +78,14 @@ articleView.handleMainNav = function() {
   // TODO: Add an event handler to nav elements that will power the Tabs feature.
   // Clicking any .tab element should hide all the .tab-content sections, and then reveal the single .tab-content section that is associated with the clicked .tab element.
   // So: You need to dynamically build a selector string with the correct ID, based on the data available to you on the .tab element that was clicked.
+  // $('.tab-content').hide();
+
+  $('nav').on('click', '.tab', function(e) {
+    e.preventDefault();
+    // $(this).data('.tab');
+    $('.tab-content').hide();
+    $(`#${$(this).data('content')}`).show();
+  });
 
   // REVIEW: Now trigger a click on the first .tab element, to set up the page.
   $('nav .tab:first').click();
@@ -78,13 +97,17 @@ articleView.setTeasers = function() {
 
   // TODO: Add an event handler to reveal all the hidden elements, when the .read-on link is clicked. You can go ahead and hide the "Read On" link once it has been clicked. Be sure to prevent the default link-click action!
   // Ideally, we'd attach this as just one event handler on the #articles section, and let it process (in other words... delegate) any .read-on clicks that happen within child nodes.
+  $('.read-on').on('click', function(e) {
+    e.preventDefault();
+    $(`#${$(this).data('.read-on')}`).slideToggle(500);
+  })
 };
 
 // TODO: Call all of the above functions, once we are sure the DOM is ready.
 $(document).ready(function() {
   articleView.populateFilters();
   articleView.handleAuthorFilter();
-  // articleView.handleCategoryFilter();
-  // articleView.handleMainNav();
+  articleView.handleCategoryFilter();
+  articleView.handleMainNav();
   articleView.setTeasers();
 })
